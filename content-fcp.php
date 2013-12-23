74e804e17b5cad9768d8abec65b08b7e3a82b045
Dan Beckett [Door4] <dan.beckett@door4.com>
2013-12-23 14:42:24 +0000

    Added a count to check if the current block is the last block so a CSS class can be conditionally added

--- content-fcp.php
+++ content-fcp.php
@@ -1,7 +1,11 @@
 <?php $flexible_content = get_field('flexible_content');
 	if($flexible_content) {
+		$i = 0;
+		$fcp_count = count($flexible_content);
 		foreach($flexible_content as $fcp) {
 			$fc_type = $fcp['acf_fc_layout'];
+			$i++;
+			$fcp_last = ($fcp_count==$i ? true : false);
 			include( 'content-fcp-'.$fc_type.'.php' );
 		};
 	}; 
